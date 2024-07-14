d285e5392d5147eba88849bb8cdb314a940224cf
Sayan Karmakar <sayancodder731@gmail.com>
2024-07-14 16:21:50 +0530

    blue selector box removed while typing

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
-let input=document.getElementById('inputBox');
-let buttons=document.querySelectorAll('button');
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
 
-let string="";
-let arr=Array.from(buttons);
-arr.forEach(buttons =>{
-    buttons.addEventListener("click", (e) => {
-        if (e.target.innerHTML == "="){
-            string= eval(string);
-            input.value=string;
-        }
-        
-        else if (e.target.innerHTML == "AC"){
-            string="";
-            input.value=string;
-        }
-
-        else if (e.target.innerHTML == "DEL"){
-            string=string.substring(0,string.length-1);
-            input.value=string;
-        }
-        else{
-            string+=e.target.innerHTML;
-        input.value=string;
-        }
-
-    })
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+  button.addEventListener("click", (e) => {
+    switch (e.target.innerHTML) {
+      case "=":
+        string = eval(string);
+        input.value = string;
+        break;
+      case "AC":
+        string = "";
+        input.value = string;
+        break;
+      case "DEL":
+        string = string.substring(0, string.length - 1);
+        input.value = string;
+        break;
+      default:
+        string += e.target.innerHTML;
+        input.value = string;
+    }
+  });
 });
